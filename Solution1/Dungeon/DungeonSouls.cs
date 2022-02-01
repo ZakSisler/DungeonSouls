01a2041a3b761afab0ebb0197e04754f8a96ee95
Zakary Sisler <zakarysisler@outlook.com>
2022-02-01 12:45:48 -0600

    Initial Commit

---
+++ Solution1/Dungeon/DungeonSouls.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    class DungeonSouls
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DUNGEON SOULS";
+            Console.WriteLine("In the Age of Ancients the world was unformed, shrouded by fog. " +
+                "\nA land of gray crags, Archtrees and Everlasting Dragons. " +
+                "\nBut then there was Fire and with fire came disparity. " +
+                "\nHeat and cold, " +
+                "\nlife and death, " +
+                "\nand of course, light and dark. " +
+                "\nThen from the dark, They came, and found the Souls of Lords within the flame. " +
+                "\n\nNito, the First of the Dead, " +
+                "\nThe Witch of Izalith and her Daughters of Chaos, " +
+                "\nGwyn, the Lord of Sunlight, and his faithful knights. " +
+                "\nAnd the Furtive Pygmy, so easily forgotten.");
+        }
+    }
+}

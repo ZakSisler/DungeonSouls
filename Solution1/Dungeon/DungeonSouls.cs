0e534ee4f32d31b781ebdac46e813ff9fd9f9e2e
Zakary Sisler <zakarysisler@outlook.com>
2022-02-13 18:06:49 -0600

    I fixed my logical errors and added some font coloring to improve the UX. Next I'll introduce the path forward if the key is chosen.

--- Solution1/Dungeon/DungeonSouls.cs
+++ Solution1/Dungeon/DungeonSouls.cs
@@ -14,8 +14,7 @@ namespace Dungeon
 
             Console.WriteLine("DUNGEON SOULS");
 
-            //Heroes made here (Perhaps they should be made in heroes.cs and called here?)
-            Heroes hero = new Heroes("Blaise Pascal", 50, 50, 50, 20, 75, "Average build, Reliable, Makes wagers");
+            
 
 
 
@@ -34,19 +33,28 @@ namespace Dungeon
 
                     //Story is introduced; A decision must be made.
                     Console.WriteLine("\n\nPiercing through the darkness, a faint light peeks through the ceiling... \n" +
-                       "\"That's strange... I don't remember getting here...\" you think to yourself. \n\n" +
+                       "\"That's strange... I don't remember getting here...\" you think to yourself.... ");
+                    Console.WriteLine("\n\nPress any key to continue..."); Console.ReadLine(); Console.Clear();
+
+                    Console.WriteLine("\n\n" +
                        "You can barely see, but you can tell your are in a cold damp room made of stone. \n" +
-                       "As your eyes adjust, you hear a small *clink* on the floor nearby, \naccompanied by the sound of rats\nchattering and scurrying away; Above, a shadowy figure lingers for a moment and then quickly disappears. \n" +
-                       "Where am I? What was that noise?\n");
+                       "As your eyes adjust, you hear a small *clink* on the floor nearby, \naccompanied by the sound of rats\nchattering and scurrying away; they look unusually large and particularly ferocious... ");
+                    Console.WriteLine("\n\nPress any key to continue..."); Console.ReadLine(); Console.Clear();
+
+                    Console.WriteLine("\n Above, a shadowy figure lingers for a moment and then quickly disappears. \n" +
+                       "\"Where am I? What was that noise?\" you think to yourself as you clutch your sides, feeling the pangs of hunger... \n");
 
 
                     //User is prompted; Only one Path 
+
                     Console.WriteLine("What do you do next?: \n\n" +
-                        "F) You quickly feel around for the item on the ground, avoiding any rats. \n(By the look of them they look unusually large and particularly ferocious...)\n" +
+                        "F) You quickly feel around for the item on the ground, avoiding any rats. \n" +
                         "R) You chase the rats... You are hungry after all...\n" +
                         "X) Exit Game");
 
-                    Console.WriteLine("\nPlease press the key of your choice");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("\n^^^Please press the key of your choice.^^^");
+                    Console.ResetColor();
                     ConsoleKey userChoice = Console.ReadKey(true).Key;
 
                     Console.Clear();
@@ -64,12 +72,27 @@ namespace Dungeon
                             if (newKey == "s")
                             {
                                 intro = true;
+                                doorOneOpened = true;
+                                
                             }
                             else
                             {
-                                exitGame = true; //TODO (Not sure if needed here, further testing needed)
-                                intro = true;
-                                doorOneOpened = true;
+                                Console.WriteLine("Game Over");
+
+                                Console.WriteLine("play again?\n\ntype \"yes\" or \"no\"");
+                                string playagain = Console.ReadLine().ToLower();
+                                if (playagain == "yes")
+                                {
+                                    Console.WriteLine("new game");
+                                    intro = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("thanks for playing!");
+                                    intro = true;
+                                    exitGame = true;
+                                    return;
+                                }
                             }
                             break;
 
@@ -77,42 +100,49 @@ namespace Dungeon
                             //This choice quickly ends the game and restarts the game; Helping the user understand the consequences of the game early on. Dungeon Souls will kick you out with no mercy.
                         case ConsoleKey.R:
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("You hastily chase the rats... annnnd to your horror are quickly swarmed and they devour you... \nYOU DIED");
+                            Console.WriteLine("You hastily chase the rats... \nto your horror you are quickly swarmed by thousands of plague infested, mutated rats and they devour you... \nYOU DIED");
                             Console.ResetColor();
                             exitGame = true;
                             intro = true;
-                            break;
+                            continue;
 
                             //Game Exit
                         case ConsoleKey.X:
                         case ConsoleKey.E:
+                            Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("\nYOU DIED (The only way to exit...)");
-                            //intro = true;
-                            break;
+                            Console.ResetColor();
+                            exitGame = true;
+                            intro = true;
+                            return;
                         default:
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
                             Console.WriteLine("Git gud. Wrong input selected.");
+                            Console.ResetColor();
                             break;
                     }
+                    break;
 
+                    //Implement second portion of the game after opening the door with the key found.
+                    //do
+                    //{
+                    //    Console.WriteLine("You found a door! You slowly open it.\n\n" +
+                    //        "You find yourself in a room with 7 doors");
+                    //    //Console.WriteLine(GetRoom());///////Remove double slash (comment out when room code is fixed)
+                    //    break;
 
-
-                    do
-                    {
-                        Console.WriteLine("You found a door! You slowly open it.\n\n" +
-                            "You find yourself in a room with 7 doors");
-                        //Console.WriteLine(GetRoom());///////Remove double slash (comment out when room code is fixed)
-                        break;
-
-                    } while (!intro && doorOneOpened);
+                    //} while (!doorOneOpened);
 
 
             } while (!exitGame);
                    
                 
+                } while (!intro && !exitGame);
                 //get this code to execute at the end of every game
-                    Console.WriteLine("play again?\n\ntype \"yes\" or \"no\"");
-                    string playagain = Console.ReadLine().ToLower();
-                    if (playagain == "yes")
+
+                    Console.WriteLine("\n\nWant play again?\n\ntype \"yes\" or \"no\"");
+                    string playagainFinal = Console.ReadLine().ToLower();
+                    if (playagainFinal == "yes")
                     {
                         Console.WriteLine("new game");
                         intro = false;
@@ -122,12 +152,14 @@ namespace Dungeon
                         Console.WriteLine("thanks for playing!");
                         intro = true;
                         exitGame = true;
-                        continue;
+                        //continue;
                     }
-                } while (!intro && !exitGame);
 
 
         }//End main
+
+
+        //Room randomizer made here
         private static string GetRoom()
         {
             string[] rooms =
